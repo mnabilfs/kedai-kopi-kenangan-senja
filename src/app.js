89e50b668c0a91e11856aa45537ec237aa6d92fd
mnabilfs <mnabilfs22@gmail.com>
2025-03-13 23:39:28 +0700

    add : shopping cart feature

--- src/app.js
+++ src/app.js
@@ -14,11 +14,57 @@ document.addEventListener('alpine:init', () => {
         total: 0,
         quantity: 0,
         add(newItem){
-            this.items.push(newItem)
-            this.quantity++
-            this.total += newItem.price
-            console.log(this.total);
+            // cek ada barang yang sama
+            const cartItem = this.items.find((item) => item.id === newItem.id)
+
+            // jika belum ada
+            if(!cartItem){
+                this.items.push({...newItem, quantity: 1, total: newItem.price})
+                this.quantity++
+                this.total += newItem.price
+            } else {
+                // jika barang ada, cek apa barang sama di cart
+                this.items = this.items.map((item) => {
+                    // jika barang berbeda
+                    if(item.id !== newItem.id){
+                        return item
+                    } else {
+                        // jika barang ada, tambah quantity dan total nya
+                        item.quantity++
+                        item.total = item.price * item.quantity
+                        this.quantity++
+                        this.total += item.price
+                        return item
+                    }
+                })
+            }
             
+        },
+        remove(id){
+            // ambil item yg mau di remove berdasarkan id
+            const cartItem = this.items.find((item) => item.id === id)
+
+            // jika item lebih dari 1
+            if (cartItem.quantity > 1) {
+                //  telusuri 1 1
+                this.items = this.items.map((item) => {
+                    // jika bukan barang yang di klik
+                    if(item.id !== id){
+                        return item
+                    } else{
+                        item.quantity--
+                        item.total = item.price * item.quantity
+                        this.quantity--
+                        this.total -= item.price
+                        return item
+                    }
+                })
+            } else if(cartItem.quantity === 1){
+                // jika barang nya sisa 1
+                this.items = this.items.filter((item) => item.id !== id)
+                this.quantity--
+                this.total -= cartItem.price
+            }
         }
     })
 })
